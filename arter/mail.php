6871c7599209813f599aee46ce77020f14b6c42a
Jecode11 <109405900+Jecode11@users.noreply.github.com>
2022-12-13 22:37:28 -0800

    Update mail.php

--- arter/mail.php
+++ arter/mail.php
@@ -8,6 +8,6 @@ $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Name: $name \nEmail: $email \nText: $text";
 
-$pagetitle = "New message from the \"$sitename\"";
+$pagetitle = "New message from the \"$sitename\"contact.html";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 ?>
